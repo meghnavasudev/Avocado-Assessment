53ca0ef7b5baaba8502efb4265cb1c6317707c46
meghnavasudev <48089022+meghnavasudev@users.noreply.github.com>
2019-02-28 06:44:50 -0500

    Add files via upload
    Assessment for Avocado to Map Values from csv file

---
+++ MapCSV.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MapCSV.aspx.cs" Inherits="Assessment.MapCSV" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+
+<head runat="server">
+    <title>Map CSV File Data on a Grid</title>
+        <meta name="viewport" content = "width = device-width, initial-scale = 1.0, minimum-scale = 1.0, maximum-scale = 1.0, user-scalable = no" />
+    <link href="bootstrap.css" rel="stylesheet" />    
+   
+</head>
+<body>
+    <form id="form2" runat="server">
+    <table style="width: 100%;">
+         <tr><td colspan="2"><h3>Solution to process CSV data and map it </h3></td></tr>
+         <tr><td><br/></td></tr>
+
+                        <tr>  
+                    <td style="width: 184px">Select CSV File:</td>  
+                    <td style="width: 331px">  
+                        <asp:FileUpload ID="FileUpload1" runat="server" />  
+                    </td>  
+                    <td>  
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="* required" ControlToValidate="FileUpload1" ForeColor="Red" ValidationGroup="validation">  
+                        </asp:RequiredFieldValidator>  
+                    </td>  
+                </tr>  
+          <tr><td><br/></td></tr>
+                <tr>  
+                    <td style="width: 184px"> </td>  
+                    <td style="width: 331px"> </td>  
+                </tr>  
+                <tr>  
+                    <td colspan="2">  
+                        <asp:Button ID="btnUpload" runat="server" Text="Upload" ValidationGroup="validation" OnClick="btnUpload_Click" />  
+                    </td>  
+                </tr>  
+            </table>  
+            <br />  
+            <asp:GridView ID="grdCSVMap"   CssClass="table table-hover table-striped" runat="server"></asp:GridView>  
+            <br />  
+            <asp:Label ID="lblerror" runat="server" />  
+    </form>
+</body>
+</html>
---
+++ MapCSV.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Reflection;
+using System.Collections;
+using System.Linq;
+
+namespace Assessment
+{
+    public partial class MapCSV : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                dt = ReadCsvFile();
+
+                //Entity Framework  LINQ and Lamda Expression
+                var result = dt
+                 .AsEnumerable()
+                .Where(myRow => myRow.Field<int>("ID") == 1001);
+
+                grdCSVMap.DataSource = dt;
+                grdCSVMap.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblerror.Text = ex.Message;
+            }
+        }
+        public DataTable ReadCsvFile()
+        {
+
+            DataTable dtCsv = new DataTable();
+            string Fulltext;
+            if (FileUpload1.HasFile)
+            {
+                string FileSaveWithPath = Server.MapPath("Employee.csv");
+                FileUpload1.SaveAs(FileSaveWithPath);
+                using (StreamReader sr = new StreamReader(FileSaveWithPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Fulltext = sr.ReadToEnd().ToString(); //read full file text  
+                        string[] rows = Fulltext.Split('\n'); //split full file text into rows  
+                        for (int i = 0; i < rows.Length - 1; i++)
+                        {
+                            string[] rowValues = rows[i].Split(','); //split each row with comma to get individual values  
+                            {
+                                if (i == 0)
+                                {
+                                    for (int j = 0; j < rowValues.Length; j++)
+                                    {
+                                        dtCsv.Columns.Add(rowValues[j]); //add headers  
+                                    }
+                                }
+                                else
+                                {
+                                    DataRow dr = dtCsv.NewRow();
+                                    for (int k = 0; k < rowValues.Length; k++)
+                                    {
+                                        dr[k] = rowValues[k].ToString();
+                                    }
+                                    dtCsv.Rows.Add(dr); //add other rows  
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return dtCsv;
+        }
+    }
+
+    //Created EmployeeValues class to Map the csv values to Business entities
+
+    class EmployeeValues
+    {
+        int ID;
+        string FirstName;
+        string LastName;
+        string Salary;
+        string City;
+
+        public static EmployeeValues FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            EmployeeValues EmployeeValues = new EmployeeValues();
+            EmployeeValues.ID = Convert.ToInt32(values[0]);
+            EmployeeValues.FirstName = Convert.ToString(values[1]);
+            EmployeeValues.LastName = Convert.ToString(values[2]);
+            EmployeeValues.City = Convert.ToString(values[3]);
+            EmployeeValues.Salary = Convert.ToString(values[4]);
+           return EmployeeValues;
+        
+    }
+}
+}
